b0cbe85dd0e5b8dc974c5305fdbfc7a7fc30784c
Brad <bycarlson23@gmail.com>
2022-07-22 18:31:27 -0500

    first starts in java, waiting for the api to load, said it may take a few hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,38 @@
 var searchHistoryList = document.getElementById('searchHistoryList');
 var searchBtn = document.getElementById('searchBtn');
 var currentWeather = document.getElementById('currentWeather');
+var forecast = document.getElementById('forecast');
 
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${WEATHER_API_KEY}&q=${city}&limit=1', {})
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
+var WEATHER_API_KEY = 'c00b8fe28f49f639215e37d6e88f2630';
+
+function getCoordinates(city) {
+    var coordinates = {
+        lon: '',
+        lat: ''
+    }
+
+
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={c00b8fe28f49f639215e37d6e88f2630}')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+};
+
+// making the search bar active w the click
+document.getElementById('searchBtn').addEventListener('click', function () {
+    event.preventDefault();
+    var cityName = $('#cityInput');
+    returnCurrentWeather(cityName);
+    returnWeatherForecast(cityName);
+});
+
+function returnCurrentWeather(cityName) {
+    let queryURL = ''
+}
+
+function returnWeatherForecast(cityName) {
+    let queryURL = ''
+}
