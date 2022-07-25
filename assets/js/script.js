078dfb1ed10a520b2d6d2ed19ae784015fda41f8
Brad <bycarlson23@gmail.com>
2022-07-25 16:37:01 -0500

    restarting my html to make it more visually understanding, started working on some java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,76 @@
-var searchHistoryList = document.getElementById('searchHistoryList');
-var searchBtn = document.getElementById('searchBtn');
-var currentWeather = document.getElementById('currentWeather');
-var forecast = document.getElementById('forecast');
+// function getCoordinates(city) {
+//     var coordinates = {
+//         lon: '',
+//         lat: ''
+//     }
+
+
+//     fetch('https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude=&appid=c00b8fe28f49f639215e37d6e88f2630')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         });
+// };
+
+// // making the search bar active w the click
+// document.getElementById('searchBtn').addEventListener('click', function () {
+//     event.preventDefault();
+//     var cityName = $('#cityInput');
+//     returnCurrentWeather(cityName);
+//     returnWeatherForecast(cityName);
+// });
+
+// function returnCurrentWeather(cityName) {
+//     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + key;
+//     console.log(queryURL);
+//     fetch(queryURL)
+//         .then(function (response) {
+//             console.log(response);
+//             cityName = response.city;
+//             weatherIcon = (response.weather.icon);
+//             weatherIcon = 'https://api.openweathermap.org/img/wn/' + weatherIcon + '@3x.png';
+//             temp = (response.main.temp);
+//             wind = (response.wind.speed);
+//             humidity = (response.main.humidity);
+//             uv = (response.main.uv);
+//             lon = (response.coordinates.lon);
+//             lat = (response.coordinates.lat);
+
+//         })
+// };
+
+// function returnWeatherForecast(cityName) {
+//     var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=&lon=&appid=' + key;
+//     console.log(queryURL);
+// };
+
+// var storedWeather = JSON.parse(localStorage.getItem('weather'));
+// var weatherSearch = [];
 
 var key = 'c00b8fe28f49f639215e37d6e88f2630';
 
-function getCoordinates(city) {
-    var coordinates = {
-        lon: '',
-        lat: ''
+function getApi() {
+    var input = $('#search');
+    var cityName = encodeURIComponent(input);
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + key
+    if (input === '') { }
+    else {
+        fetch(queryURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+
+                var mainCity = data.name
+                city.append(mainCity);
+
+                var mainTemp = data.main.temp;
+                temp.append(mainTemp);
+
+
+            })
     }
-
-
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude=&appid=c00b8fe28f49f639215e37d6e88f2630')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-};
-
-// making the search bar active w the click
-document.getElementById('searchBtn').addEventListener('click', function () {
-    event.preventDefault();
-    var cityName = $('#cityInput');
-    returnCurrentWeather(cityName);
-    returnWeatherForecast(cityName);
-});
-
-function returnCurrentWeather(cityName) {
-    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + key;
-    console.log(queryURL);
-    fetch(queryURL)
-        .then(function (response) {
-            console.log(response);
-            cityName = response.city;
-            weatherIcon = 
-        })
-};
-
-function returnWeatherForecast(cityName) {
-    var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=&lon=&appid=' + key;
-    console.log(queryURL);
-};
-
-var storedWeather = JSON.parse(localStorage.getItem('weather'));
-var weatherSearch = [];
+}
