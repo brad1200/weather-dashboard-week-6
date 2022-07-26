8b975d98b221f08d1e7bb71e8f9d93d4127dc1dc
Brad <bycarlson23@gmail.com>
2022-07-26 17:53:33 -0500

    final upate before turning in, not complete, html should be good just java is not done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,9 @@
-// function getCoordinates(city) {
-//     var coordinates = {
-//         lon: '',
-//         lat: ''
-//     }
-
-
-//     fetch('https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude=&appid=c00b8fe28f49f639215e37d6e88f2630')
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         });
-// };
-
-// // making the search bar active w the click
-// document.getElementById('searchBtn').addEventListener('click', function () {
-//     event.preventDefault();
-//     var cityName = $('#cityInput');
-//     returnCurrentWeather(cityName);
-//     returnWeatherForecast(cityName);
-// });
-
-// function returnCurrentWeather(cityName) {
-//     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + key;
-//     console.log(queryURL);
-//     fetch(queryURL)
-//         .then(function (response) {
-//             console.log(response);
-//             cityName = response.city;
-//             weatherIcon = (response.weather.icon);
-//             weatherIcon = 'https://api.openweathermap.org/img/wn/' + weatherIcon + '@3x.png';
-//             temp = (response.main.temp);
-//             wind = (response.wind.speed);
-//             humidity = (response.main.humidity);
-//             uv = (response.main.uv);
-//             lon = (response.coordinates.lon);
-//             lat = (response.coordinates.lat);
-
-//         })
-// };
-
-// function returnWeatherForecast(cityName) {
-//     var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=&lon=&appid=' + key;
-//     console.log(queryURL);
-// };
-
-// var storedWeather = JSON.parse(localStorage.getItem('weather'));
-// var weatherSearch = [];
+function getCoordinates(city) {
+    var coordinates = {
+        lon: '',
+        lat: ''
+    }
+};
 
 var key = 'c00b8fe28f49f639215e37d6e88f2630';
 
@@ -73,4 +29,4 @@ function getApi() {
 
             })
     }
-}
+};
